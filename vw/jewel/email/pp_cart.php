b28eb3d981b83e247703adf77c83eb158fbbcfb7
Owner <Owner@696908E74163425>
2009-06-15 21:42:58 -0600

    Paypal cart transaction email body

--- vw/jewel/email/pp_cart.php
+++ vw/jewel/email/pp_cart.php
@@ -1,25 +1,29 @@
 <?php
-/* Do not allow hacker to use this to send emails */
-if ($key != 19600324) return;
-//	$email = 'jane@jenandjane.com';
-	$subject = "JJ PayPal $txnType Transaction: ";
+/* Email subject and body - caller must capture with ob_start() */
+	$subject = "JJ PayPal $txnType Transaction: $first_name $last_name";
 	// format $emailtext from transaction
-	$txt = '';
-	$txt .= "$first_name $last_name, $payer_email \t ($payer_status) \n";
-	$txt .= " $payment_gross  Shipping: $mc_shipping Fee: $mc_fee \n";
-	$txt .= " Payment: $mc_shipping \n";
-	$txt .= "\n";
-	$txt .= "Ship To: \t ($address_status) \n";
-	$txt .= " $address_name \n";
-	$txt .= " $address_street \n";
-	$txt .= " $address_city, $address_state $address_zip \n";
-	$txt .= "\n";
-	for ($i = 1; $i < $num_cart_items; $i++) {
-		$txt .= "$i. ${'item_number'.$i} ${'item_name'.$i} ${'mc_gross_'.$i} \n ";
-		$txt .= "  Tax: ${'tax'.$i}  Shipping: ${'mc_shipping'.$i} \n\n";
+	echo " We have a new order through PayPal! \n\n";
+	echo "$first_name $last_name, $payer_email, $contact_phone \t ($payer_status) \n";
+	echo " Currency:            ", $mc_currency, "\n";
+	echo " Full payment amount: ", number_format($mc_gross, 2), " \n";
+	echo " Shipping:            ", number_format($mc_shipping, 2), "\n";
+	echo " Transaction fee:     ", number_format($mc_fee, 2), " \n";
+	echo "\n";
+	echo "Ship To: \t ($address_status) \n";
+	echo " $address_name \n";
+	echo " $address_street \n";
+	echo " $address_city, $address_state $address_zip \n";
+	echo "\n";
+	echo "Customer note: \n";
+	echo $memo, "\n";
+	echo "\n";
+	echo "Order: \n";
+	for ($i = 1; $i <= $num_cart_items; $i++) {
+		$gross = ${'mc_gross_'.$i};
+		$ship = ${'mc_shipping'.$i};
+		echo "$i. ${'quantity'.$i} x ${'item_number'.$i} ${'item_name'.$i}  \n ";
+		echo "   ${'option_name'.$i}   ${'option_selection'.$i} ", 
+				'  Gross: $', number_format($gross, 2), 
+				'  Shipping: $', number_format($ship, 2), "\n\n";
 	}
-	$headers = 'From: andre@jenandjane.com' . "\r\n" .
-    	'Reply-To: andre@jenandjane.com' . "\r\n" .
-    	'X-Mailer: PHP/' . phpversion();
-	mail($email, $subject, $txt . "\n\n", $headers );
 ?>
