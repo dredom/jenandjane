c0d27cd7b869859a7000a855bc5d2fbb585e00ca
Owner <Owner@696908E74163425>
2009-07-30 21:19:16 -0600

    Turn on production PayPal

--- shop/shop.config.php
+++ shop/shop.config.php
@@ -5,10 +5,10 @@ if (ENV == 'development') {
 	define('shop_paypal_sandbox', 'sandbox.');	// www.sandbox.paypal.com
 } else {	// production
 	define('shop_email_to', 'jane@jenandjane.com');
-	//define('shop_biz_email', 'jane@jenandjane.com');
-	define('shop_biz_email', 'untied_1238972877_biz@gmail.com'); // testing
-	//define('shop_paypal_sandbox', '');
-	define('shop_paypal_sandbox', 'sandbox.');	// testing
+	define('shop_biz_email', 'jane@jenandjane.com');
+//	define('shop_biz_email', 'untied_1238972877_biz@gmail.com'); // testing
+	define('shop_paypal_sandbox', '');
+//	define('shop_paypal_sandbox', 'sandbox.');	// testing
 }
 define('shop_email_fail_to', 'untiedt@live.com');
 define('shop_email_from', 'paypal@jenandjane.com');
