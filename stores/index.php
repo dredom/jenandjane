4a41767b92383d6c27162ff3803d9beb1d48cd94
Owner <Owner@696908E74163425>
2009-08-04 20:23:52 -0600

    Remove gone stores; add The Loft.

--- stores/index.php
+++ stores/index.php
@@ -5,7 +5,7 @@
  <link rel=StyleSheet href="/css/style.css" type="text/css">
  <style type="text/css">
 <!--
- LI {font-size:12pt; font-weight:bold; color:#FFCCCC}
+ LI SPAN {font-size:12pt; font-weight:bold; color:#FFCCCC}
 -->
  </style>
 </head>
@@ -32,102 +32,55 @@
 
 <ul type=circle>
 
- <li>Studio City, CA: </li>
+ <li><span>Studio City, CA:</span>
 
-<p>
+	<br><br>
+ * Our partner store *<br>
  <big>
- <b>* Our new partner store *</b><br>
   POMM <br>
   4330 Tujunga Avenue
   <br>
   Studio City,  CA 91604
   <br>
-  (818) 752-7521
+  818-752-7521
   </big>
-  <div align="center">
+  <div align="center" style="margin-top: 5px">
 <div style="margin-left:auto; margin-right:auto; width:640px; background-color: #990000; color: #ffbebe; border-style: double;">
- <a href="http://www.jenandjane.com/stores" title="store locations" ><img
-  src="http://www.jenandjane.com/img/pommstore1.jpg" border="0" alt="POMM store"></a>
+ <img src="http://www.jenandjane.com/img/pommstore1.jpg" border="0" alt="POMM store">
 </div>
-</div>
+  </div>
   
-</p>
+	<br><br>
+ </li>
 
 
- <li>Bevery Hills, CA </li>
+ <li><span>Manhattan Beach, CA:</span>
+	<br><br>
+The Loft <br>
+1015 Manhattan Avenue <br>
+Manhattan Beach, CA 90266 <br>
+818-297-9000 <br>
+	<br><br>
+  </li>
 
-<p>
-David’s Shoe Salon <br>
-373 No. Bedford Dr. <br>
-Beverly Hills, CA 90210 <br>
-310-273-4075
-</p>
-
- <li>Calabasas, CA: </li>
-
-<p>
-David’s Shoe Salon <br>
-Commons at Calabasas <br>
-4732 Commons Way <br>
-Calabasas, Ca  91302 <br>
-818-222-2106
-</p>
-
- <li>Encino, CA: </li>
-
-<p>
-David’s Shoe Salon <br>
-Encino Place <br>
-16101 Ventura Boulevard <br>
-Encino, CA  91436 <br>
-818-789-1493
-</p>
-
- <li>Houston, TX: </li>
-
-<p>
-Aunt Mike’s in the Heights <br>
-3320 White Oak Drive <br>
-Houston, TX  77077 <br>
-713-802-1830
-</p>
-
- <li>Laguna Beach, CA: </li>
-
-<p>
-The Montage Resort & Spa <br>
+ <li><span>Laguna Beach, CA:</span> 
+	<br><br>
+The Montage Resort &amp; Spa <br>
 Treasures <br>
 30801 South Coast Highway <br>
 Laguna Beach, CA 92651 <br>
-949-715-6196
-</p>
+949-715-6196 <br>
+	<br><br>
+ </li>
 
- <li>Newport Beach, CA: </li>
-
-<p>
+ <li><span>Newport Beach, CA: </span>
+	<br><br>
 Trompe L’Oeil <br>
 413 29th Street <br>
 Newport Beach, CA 92663 <br>
-949-675-2024
-</p>
-<p>
-The Pauline Nelson Collection of Fine Jewelry <br>
-2421 16th Street <br>
-New port Beach, CA 92663 <br>
-949-645-4605 <br>
-(By Appointment)
-</p>
-
- <li>Westlake Village, CA: </li>
-
-<p>
-David’s Shoe Salon <br>
-Promenade at Westlake  <br>
-140 Promenade Way <br>
-Westlake Village, CA 91362 <br>
-805-374-9949
-</p>
-
+949-675-2024 <br>
+	<br><br>
+ </li>
 
 </ul>
 </div>
